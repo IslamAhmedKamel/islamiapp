dcc093a19c298f8f076a80cc677f436740817cdc
IslamAhmedKamel <https://github.com/IslamAhmedKamel>
2026-04-20 16:28:52 +0200

    تعديل الملفات والاضافات

---
+++ lib/l10n/app_localizations_ar.dart
@@ -0,0 +1,58 @@
+// ignore: unused_import
+import 'package:intl/intl.dart' as intl;
+import 'app_localizations.dart';
+
+// ignore_for_file: type=lint
+
+/// The translations for Arabic (`ar`).
+class AppLocalizationsAr extends AppLocalizations {
+  AppLocalizationsAr([String locale = 'ar']) : super(locale);
+
+  @override
+  String get appTitle => 'إسلامى';
+
+  @override
+  String get suraName => 'SuraName';
+
+  @override
+  String get ahadeths => 'الاحاديث';
+
+  @override
+  String get english => 'إنجليزى';
+
+  @override
+  String get arabic => 'عربى';
+
+  @override
+  String get mode => 'الوضع';
+
+  @override
+  String get language => 'اللغه';
+
+  @override
+  String get modeLight => 'أبيض';
+
+  @override
+  String get modeDark => 'مظلم';
+
+  @override
+  String get quran => 'القرآن';
+
+  @override
+  String get sebha => 'سبحة';
+
+  @override
+  String get radio => 'الراديو';
+
+  @override
+  String get radioHoly => 'إذاعة القرآن الكريم';
+
+  @override
+  String get tsbeh => 'عدد التسبيحات';
+
+  @override
+  String get setting => 'الاعدادات';
+
+  @override
+  String get devApp => 'معلومات مطور التطبيق';
+}
---
+++ lib/l10n/app_localizations_en.dart
@@ -0,0 +1,58 @@
+// ignore: unused_import
+import 'package:intl/intl.dart' as intl;
+import 'app_localizations.dart';
+
+// ignore_for_file: type=lint
+
+/// The translations for English (`en`).
+class AppLocalizationsEn extends AppLocalizations {
+  AppLocalizationsEn([String locale = 'en']) : super(locale);
+
+  @override
+  String get appTitle => 'Islami';
+
+  @override
+  String get suraName => 'SuraName';
+
+  @override
+  String get ahadeths => 'Ahadeths';
+
+  @override
+  String get english => 'English';
+
+  @override
+  String get arabic => 'Arabic';
+
+  @override
+  String get mode => 'Mode';
+
+  @override
+  String get language => 'Language';
+
+  @override
+  String get modeLight => 'Light';
+
+  @override
+  String get modeDark => 'Dark';
+
+  @override
+  String get quran => 'Quraan';
+
+  @override
+  String get sebha => 'Sebha';
+
+  @override
+  String get radio => 'Radio';
+
+  @override
+  String get radioHoly => 'Holy Quran Radio';
+
+  @override
+  String get tsbeh => 'Number of praises';
+
+  @override
+  String get setting => 'Settings';
+
+  @override
+  String get devApp => 'info to Developer';
+}
